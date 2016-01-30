ccc78322c15e3b651d9da4ee90fa4132d610f9b6
Adam Vegh <veghadam1991@gmail.com>
2016-01-30 07:28:53 +0100

    add median.py

---
+++ median.py
@@ -0,0 +1,9 @@
+﻿__author__ = "Vegh Adam"
+
+def checkio(data):
+    data.sort()
+    if len(data) % 2 == 0:
+        med = (data[(len(data)-1) // 2] + data[(len(data)-1) // 2 + 1]) / 2
+    else:
+        med = data[(len(data)-1) // 2]
+    return med

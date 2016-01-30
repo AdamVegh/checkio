f4cefe9bf0f1f0447750d5c3553393b910b709d8
Adam Vegh <veghadam1991@gmail.com>
2016-01-30 07:36:28 +0100

    add house_password.py

---
+++ house_password.py
@@ -0,0 +1,14 @@
+﻿__author__ = 'Vegh Adam'
+
+def checkio(data):
+    if len(data) < 10:
+        return False
+    is_digit, is_upper, is_lower = 0, 0, 0
+    for i in data:
+        if i.isdigit():
+            is_digit += 1
+        elif i.isupper():
+            is_upper += 1
+        elif i.islower():
+            is_lower += 1
+    return is_digit * is_upper * is_lower

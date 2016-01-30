5c12ff0ff9792a34670e1c0512b7cf0bbd624670
Adam Vegh <veghadam1991@gmail.com>
2016-01-30 08:18:01 +0100

    add solution for anything.py

---
+++ solution for anything.py
@@ -0,0 +1,18 @@
+__author__ = 'Vegh Adam'
+
+class Solution:
+    def __eq__(self, other):
+        return True
+    def __ne__(self, other):
+        return True
+    def __lt__(self, other):
+        return True
+    def __gt__(self, other):
+        return True
+    def __le__(self, other):
+        return True
+    def __ge__(self, other):
+        return True
+
+def checkio(anything):
+    return Solution()

9788f62b0c839b24cd536b1209b8ae0ac756041f
Adam Vegh <veghadam1991@gmail.com>
2016-02-08 16:07:19 +0100

    add fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+__author__ = 'Vegh Adam'
+
+def checkio(number):
+    if number % 15 == 0:
+        return 'Fizz Buzz'
+    elif number % 3 == 0:
+        return 'Fizz'
+    elif number % 5 == 0:
+        return 'Buzz'
+    return str(number)
